c3110ac2fc41f46f86ad5c853d1261612f4e9daa
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-01-08 21:33:01 -0400

    Added record-type.sql script.

---
+++ pl-sql/record-type.sql
@@ -0,0 +1,24 @@
+SET SERVEROUTPUT ON
+
+DECLARE
+    TYPE employeeRecordType IS RECORD (
+        name VARCHAR2(46),
+        phone employees.phone_number%TYPE,
+        email employees.email%TYPE);
+    vEmployeeRecord employeeRecordType;
+BEGIN
+    SELECT
+        first_name || ' ' || last_name,
+        phone_number,
+        email
+    INTO 
+        vEmployeeRecord.name,
+        vEmployeeRecord.phone,
+        vEmployeeRecord.email
+    FROM employees
+    WHERE employee_id = 170;
+
+    DBMS_OUTPUT.PUT_LINE('Name..: ' || vEmployeeRecord.name);
+    DBMS_OUTPUT.PUT_LINE('Phone.: ' || vEmployeeRecord.phone);
+    DBMS_OUTPUT.PUT_LINE('E-mail: ' || vEmployeeRecord.email);
+END;
