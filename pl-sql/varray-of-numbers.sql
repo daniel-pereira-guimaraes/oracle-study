f29c8c17c988d78f358d2aa06bb55cdbb03ebdab
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-01-09 21:51:30 -0400

    Added varray-of-numbers.sql script.

---
+++ pl-sql/varray-of-numbers.sql
@@ -0,0 +1,15 @@
+SET SERVEROUTPUT ON
+
+DECLARE
+    TYPE number_table_type IS VARRAY(10) OF NUMBER(4);
+    number_table number_table_type := number_table_type();
+BEGIN
+    FOR i IN 1..10 LOOP
+        number_table.extend;
+        number_table(i) := DBMS_RANDOM.VALUE(1000, 2000);
+    END LOOP;
+
+    FOR i IN number_table.FIRST..number_table.LAST LOOP
+        DBMS_OUTPUT.PUT_LINE(TRIM(TO_CHAR(i, '00')) || ': ' || number_table(i));
+    END LOOP;
+END;
