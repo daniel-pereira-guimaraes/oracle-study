1f4e5a2bf7fd0d73242ad5eb0c1e5fca86c7bca9
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-01-09 21:58:23 -0400

    Update varray-with-rowtype-and-bulk-collect.sql script.

--- pl-sql/varray-with-rowtype-and-bulk-collect.sql
+++ pl-sql/varray-with-rowtype-and-bulk-collect.sql
@@ -1,16 +1,16 @@
 SET SERVEROUTPUT ON
 
 DECLARE
-    TYPE countries_varray IS VARRAY(100) OF countries%rowtype;
-    countries_table countries_varray := countries_varray();
+    TYPE countries_varray_type IS VARRAY(100) OF countries%rowtype;
+    countries_varray countries_varray_type := countries_varray_type();
 BEGIN
-    SELECT * BULK COLLECT INTO countries_table 
+    SELECT * BULK COLLECT INTO countries_varray 
     FROM countries 
     ORDER BY country_id;
     
-    FOR i IN countries_table.FIRST..countries_table.LAST LOOP
+    FOR i IN countries_varray.FIRST..countries_varray.LAST LOOP
         DBMS_OUTPUT.PUT_LINE(
-            countries_table(i).country_id || ' - ' || 
-            countries_table(i).country_name);
+            countries_varray(i).country_id || ' - ' || 
+            countries_varray(i).country_name);
     END LOOP;
 END;
