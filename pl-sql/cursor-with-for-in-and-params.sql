9c65debe1ffc1c031220ca8cbc149c94ed1e308d
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-01-10 19:05:50 -0400

    Added cursor-with-for-in-and-params.sql script.

---
+++ pl-sql/cursor-with-for-in-and-params.sql
@@ -0,0 +1,18 @@
+SET SERVEROUTPUT ON
+
+DECLARE
+    CURSOR countriesCursor(pRegionId INTEGER) IS 
+        SELECT 
+            c.country_name, 
+            r.region_name
+        FROM countries c
+        JOIN regions r ON r.region_id = c.region_id
+        WHERE r.region_id = pRegionId
+        ORDER BY c.country_name;
+BEGIN
+    FOR countryRecord IN countriesCursor(2) LOOP
+        DBMS_OUTPUT.PUT_LINE(
+            countryRecord.region_name || ' - ' ||
+            countryRecord.country_name);
+    END LOOP;
+END;
