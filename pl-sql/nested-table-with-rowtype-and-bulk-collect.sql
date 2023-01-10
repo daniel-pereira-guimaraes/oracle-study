2f9f50d6929825240df4900aa633889e88df93c9
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-01-09 21:42:35 -0400

    Added nested-table-with-rowtype-and-bulk-collect.sql script.

---
+++ pl-sql/nested-table-with-rowtype-and-bulk-collect.sql
@@ -0,0 +1,16 @@
+SET SERVEROUTPUT ON
+
+DECLARE
+    TYPE countries_table_type IS TABLE OF countries%rowtype;
+    countries_table countries_table_type := countries_table_type();
+BEGIN
+    SELECT * BULK COLLECT INTO countries_table 
+    FROM countries 
+    ORDER BY country_id;
+    
+    FOR i IN countries_table.FIRST..countries_table.LAST LOOP
+        DBMS_OUTPUT.PUT_LINE(
+            countries_table(i).country_id || ' - ' || 
+            countries_table(i).country_name);
+    END LOOP;
+END;
