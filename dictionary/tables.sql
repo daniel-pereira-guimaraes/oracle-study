e7a835f7b6150937fc872a00540ce3938ab42881
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-01-12 21:31:21 -0400

    Added tables.sql script.

---
+++ dictionary/tables.sql
@@ -0,0 +1,28 @@
+DESC tableName;
+
+SELECT * FROM user_objects 
+WHERE object_type = 'TABLE';
+
+SELECT object_name FROM user_objects 
+WHERE object_type = 'TABLE';
+
+SELECT
+	c.column_id, 
+	c.column_name, 
+	c.data_type, 
+	c.data_length, 
+	c.data_precision, 
+	c.data_scale, 
+	c.nullable
+FROM sys.all_tab_columns c
+INNER JOIN sys.all_tables t on c.owner = t.owner and c.table_name = t.table_name
+WHERE c.owner = 'HR' and c.table_name = 'REGIONS'
+ORDER BY c.column_id;
+
+SELECT
+    owner,
+    table_name, 
+    tablespace_name,
+    status,
+    num_rows
+FROM sys.all_tables;
