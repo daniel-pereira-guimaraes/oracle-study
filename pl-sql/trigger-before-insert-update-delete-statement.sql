e22f48e6a75cd74f0384eb9f159d7afc977e2164
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-01-14 19:00:42 -0400

    Added trigger-before-insert-update-delete-statement.sql script.

---
+++ pl-sql/trigger-before-insert-update-delete-statement.sql
@@ -0,0 +1,10 @@
+-- biuds = before, insert, update, delete, statement
+CREATE OR REPLACE TRIGGER tg_bu_employees_salary_inc
+    BEFORE INSERT OR UPDATE OR DELETE ON employees
+BEGIN
+    IF TO_NUMBER(TO_CHAR(SYSDATE, 'HH24')) NOT BETWEEN 8 AND 18 OR
+        TO_NUMBER(TO_CHAR(SYSDATE, 'D')) NOT BETWEEN 2 AND 6 
+    THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Perform this operation from Monday to Friday, from 8:00 am to 6:00 pm.');
+    END IF;
+END;
