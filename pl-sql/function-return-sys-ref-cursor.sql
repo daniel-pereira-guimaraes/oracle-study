7a474c8dbc7f71b6f88ebab441e8f2d1842f28b9
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-01-14 19:45:21 -0400

    Added function-return-sys-ref-cursor.sql script.

---
+++ pl-sql/function-return-sys-ref-cursor.sql
@@ -0,0 +1,26 @@
+SET SERVEROUTPUT ON;
+
+CREATE OR REPLACE FUNCTION fn_cursor_employees(p_job_id employees.job_id%TYPE) 
+    RETURN SYS_REFCURSOR
+AS
+    v_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN v_cursor FOR SELECT first_name, last_name FROM employees;
+    RETURN v_cursor;
+END;
+
+CREATE OR REPLACE PROCEDURE sp_call_fn_cursor_employees
+AS
+    v_cursor SYS_REFCURSOR;
+    v_first_name employees.first_name%TYPE;
+    v_last_name employees.last_name%TYPE;
+BEGIN
+    v_cursor := fn_cursor_employees('IT_PROG');
+    LOOP
+        FETCH v_cursor INTO v_first_name, v_last_name;
+        EXIT WHEN v_cursor%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE(v_first_name || ' - ' || v_last_name);
+    END LOOP;
+END;
+
+EXEC sp_call_fn_cursor_employees;
