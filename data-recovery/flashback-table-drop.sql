9b0ad04a7c4db194e6d2330ecff397ac95a5a888
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-01-15 20:00:45 -0400

    Added flashback-table-drop.sql script.

---
+++ data-recovery/flashback-table-drop.sql
@@ -0,0 +1,16 @@
+CREATE TABLE flashback_drop_test(test_id INTEGER, test_name VARCHAR2(20));
+
+INSERT INTO flashback_drop_test VALUES(1, 'First');
+INSERT INTO flashback_drop_test VALUES(2, 'Second');
+
+SELECT * FROM flashback_drop_test;
+
+DROP TABLE flashback_drop_test;
+
+SELECT * FROM user_recyclebin ORDER BY droptime DESC;
+
+FLASHBACK TABLE flashback_drop_test TO BEFORE DROP;
+
+SELECT * FROM user_recyclebin ORDER BY droptime DESC;
+
+SELECT * FROM flashback_drop_test;
