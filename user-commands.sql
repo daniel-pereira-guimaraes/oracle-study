751115ae4f6b895211fb4077a6c4de43d9495366
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-01-19 07:12:29 -0400

    Update user-commands.sql script.

--- user-commands.sql
+++ user-commands.sql
@@ -1,2 +1,12 @@
 -- Show connected user:
-show user;
+SHOW USER;
+
+-- Create a new user
+CREATE USER username
+IDENTIFIED BY password
+DEFAULT TABLESPACE users
+TEMPORARY TABLESPACE temp
+QUOTA UNLIMITED ON users;
+
+-- Grant connect
+GRANT CREATE SESSION TO username;
