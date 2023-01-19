7c8cf6c6da63087300e5353d06c75b3d0cf4bb5d
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-01-19 07:21:23 -0400

    Update create-drop-synonym.sql script.

--- ddl/create-drop-synonym.sql
+++ ddl/create-drop-synonym.sql
@@ -1,9 +1,14 @@
+-- As DBA:
 CREATE SYNONYM emp FOR employees;
 
 SELECT * FROM emp;
 
+-- As DBA:
 DROP SYNONYM emp;
 
+-- As DBA
 CREATE PUBLIC SYNONYM emp FOR hr.employees;
-
 DROP PUBLIC SYNONYM emp;
+
+-- As DBA:
+CREATE SYNONYM user1.emp FOR user2.tablename;
