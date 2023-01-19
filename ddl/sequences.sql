0d897d15f5a8f519b5a0c9bc50a5030df35307b5
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-01-18 21:10:10 -0400

    Update sequences.sql script.

--- ddl/sequences.sql
+++ ddl/sequences.sql
@@ -2,6 +2,8 @@ CREATE SEQUENCE sq_example
 INCREMENT BY 2
 START WITH 1;
 
+ALTER SEQUENCE sq_example INCREMENT BY 1;
+
 SELECT sq_example.nextval FROM dual;
 
 SELECT sq_example.currval FROM dual; -- Require previos nextval access.
