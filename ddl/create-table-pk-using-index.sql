3575007705dfac8ab72118b3266ee5156682014a
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-01-21 20:08:17 -0400

    Update create-table-pk-using-index.sql script.

--- ddl/create-table-pk-using-index.sql
+++ ddl/create-table-pk-using-index.sql
@@ -4,3 +4,15 @@ CREATE TABLE person(
         USING INDEX (CREATE INDEX idx_person_id ON person(id)),
     first_name VARCHAR2(20) NOT NULL
 );
+
+-- OR --
+
+CREATE TABLE person(
+    id NUMBER(6) NOT NULL,
+    first_name VARCHAR2(20) NOT NULL
+);
+
+CREATE INDEX idx_person_id ON person(id);
+
+ALTER TABLE person
+ADD CONSTRAINT pk_person PRIMARY KEY(Id) USING INDEX idx_person_id;
