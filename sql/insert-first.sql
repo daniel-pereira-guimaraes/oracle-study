a5d6b68d2b7df36ef3c532235dd048bb7b29c72d
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-01-21 21:16:51 -0400

    Added insert-first.sql script.

---
+++ sql/insert-first.sql
@@ -0,0 +1,32 @@
+CREATE TABLE no_salary(
+    first_name VARCHAR2(20) NOT NULL
+);
+
+CREATE TABLE low_salary(
+    first_name VARCHAR2(20) NOT NULL,
+    salary NUMBER(8,2) NOT NULL
+);
+
+CREATE TABLE middle_salary(
+    first_name VARCHAR2(20) NOT NULL,
+    salary NUMBER(8,2) NOT NULL
+);
+
+CREATE TABLE high_salary(
+    first_name VARCHAR2(20) NOT NULL,
+    salary NUMBER(8,2) NOT NULL
+);
+
+INSERT FIRST
+    WHEN salary IS NULL OR salary <= 0 THEN
+        INTO no_salary(first_name) VALUES(first_name)
+    WHEN salary <= 5000 THEN
+        INTO low_salary(first_name, salary) VALUES(first_name, salary)
+    WHEN salary <= 10000 THEN
+        INTO middle_salary(first_name, salary) VALUES(first_name, salary)
+    ELSE
+        INTO high_salary(first_name, salary) VALUES(first_name, salary)
+SELECT
+    first_name, 
+    salary
+FROM employees;
