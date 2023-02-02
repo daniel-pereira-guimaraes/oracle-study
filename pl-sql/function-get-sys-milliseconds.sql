12fc73e4eb61f98bcbce703cbcf81f55a3271f17
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-02-01 20:15:50 -0400

    Added nls-date-time-format.sql script.

--- pl-sql/function-get-sys-milliseconds.sql
+++ pl-sql/function-get-sys-milliseconds.sql
@@ -1,24 +1,25 @@
-CREATE OR REPLACE FUNCTION fn_get_sys_milliseconds RETURN NUMBER 
+CREATE OR REPLACE FUNCTION fn_milliseconds_between(
+    a TIMESTAMP, b TIMESTAMP) 
+    RETURN NUMBER 
 AS
-    t TIMESTAMP := SYSTIMESTAMP;
 BEGIN
-    RETURN EXTRACT(DAY FROM t) * 24 * 60 * 60 * 1000 +
-        EXTRACT(HOUR FROM t) * 60 * 60 * 1000 +
-        EXTRACT(MINUTE FROM t) * 60 * 1000 +
-        EXTRACT(SECOND FROM t) * 1000;
+    RETURN EXTRACT(DAY FROM (b-a)) * 24 * 60 * 60 * 1000 +
+        EXTRACT(HOUR FROM (b-a)) * 60 * 60 * 1000 +
+        EXTRACT(MINUTE FROM (b-a)) * 60 * 1000 +
+        EXTRACT(SECOND FROM (b-a)) * 1000;
 END;
 
 DECLARE
-    t1 NUMBER;
-    t2 NUMBER;
+    t1 TIMESTAMP;
+    t2 TIMESTAMP;
 BEGIN
-    t1 := fn_get_sys_milliseconds();
+    t1 := SYSTIMESTAMP;
     FOR r IN (SELECT first_name FROM employees) LOOP
         DBMS_OUTPUT.PUT_LINE(r.first_name);   
     END LOOP;
-    t2 := fn_get_sys_milliseconds();
+    t2 := SYSTIMESTAMP;
     DBMS_OUTPUT.PUT_LINE('t1: ' || t1);
     DBMS_OUTPUT.PUT_LINE('t2: ' || t2);
-    DBMS_OUTPUT.PUT_LINE('t2-t1: ' || (t2 - t1));
+    DBMS_OUTPUT.PUT_LINE('t2-t1: ' || fn_milliseconds_between(t1, t2) || 'ms');
 END;
 
